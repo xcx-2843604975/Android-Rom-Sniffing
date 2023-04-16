32a68a9184c5479cd40130eb15b152a46963c95b
xcx-2843604975 <78586421+xcx-2843604975@users.noreply.github.com>
2023-04-16 18:03:13 +0800

    Add files via upload

---
+++ a.py
@@ -0,0 +1,16 @@
+from requests import post
+from os import system
+fid = input('Input fid:')
+headers = {'Content-Type':'application/x-www-form-urlencoded'}
+data = {'submit':'submit',
+        'action':'getdownloadmirrors',
+        'fid':fid}
+ret = post(url = 'https://androidfilehost.com/libs/otf/mirrors.otf.php', data = data, headers = headers)
+content = ret.json()
+if len(content['MIRRORS']) < 1:
+    print('No download link available.')
+    exit()
+for i in content['MIRRORS']:
+    print(f"Server name: {i['name']}")
+    print(f"Download link: {i['url']}\r\n")
+system('pause')
